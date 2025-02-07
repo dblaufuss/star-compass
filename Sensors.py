4b5e22ffc587eb03628172aaf056b98ea7e948e2
dblaufuss <dblaufuss06@gmail.com>
2025-02-07 11:40:12 -0500

    udpate sensors + skymap

--- Sensors.py
+++ Sensors.py
@@ -1,25 +1,21 @@
 import websocket
 import json
 
-def get_orientation(host: str) -> dict:
+def get_sensor(target: str, host: str = "ws://localhost:8080") -> dict:
     ws = websocket.WebSocket()
-    ws.connect(f"{host}/sensor/connect?type=android.sensor.orientation")
+    ws.connect(f"{host}{target}")
     try:
         return json.loads(ws.recv())
     finally:
         ws.close()
 
-def get_location(host: str) -> dict:
-    ws = websocket.WebSocket()
-    ws.connect(f"{host}/gps")
-    try:
-        return json.loads(ws.recv())
-    finally:
-        ws.close()
+def get_orientation() -> dict:
+    return get_sensor("/sensor/connect?type=android.sensor.orientation")
+
+def get_location() -> dict:
+    return get_sensor("/gps")
 
 if __name__ == "__main__":
-    server = "ws://localhost:8080"
-
     while True:
-        print(get_orientation(server))
-        print(get_location(server))
+        print(get_orientation())
+        print(get_location())
--- Skymap.py
+++ Skymap.py
@@ -4,55 +4,61 @@ from astropy.coordinates import *
 from astropy.time import Time
 import astropy.units as u
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
+import matplotlib.animation as animation
 import numpy as np
 
-time = Time.now()
-sun = get_sun(time)
+def get_stars(time: Time, location: EarthLocation) -> np.array:
+    with load.open(hipparcos.URL) as f:
+        stars_raw = hipparcos.load_dataframe(f)
 
-observer = EarthLocation(lat=38.9864*u.deg, lon=-76.8657*u.deg)
+    data = [[],[],[]]
 
-sun_coord = SkyCoord(sun.ra, sun.dec)
+    for index, star in stars_raw.iterrows():
+        if star["magnitude"] > 3:
+            continue
 
-test_coord = SkyCoord(320.69621276, -15.35417026, unit=u.deg)
+        altaz = SkyCoord(
+            star["ra_degrees"],
+            star["dec_degrees"],
+            unit=u.deg
+        ).transform_to(AltAz(location=location, obstime=time))
 
+        alt = float((altaz.alt*u.deg).value)
+        az = float((altaz.az*u.deg).value)
 
-with load.open(hipparcos.URL) as f:
-    stars = hipparcos.load_dataframe(f)
+        if alt < 0:
+            continue
 
-stars_mag = []
-stars_alt = []
-stars_az = []
+        data[0].append(star["magnitude"])
+        data[1].append(alt)
+        data[2].append(az)
 
-for index, star in stars.iterrows():
-    if star["magnitude"] >= 3.5:
-        continue
+    return np.array(data)
 
-    altaz = SkyCoord(star["ra_degrees"], star["dec_degrees"], unit=u.deg).transform_to(AltAz(location=observer, obstime=time))
-    alt = float((altaz.alt*u.deg).value)
-    az = float((altaz.az*u.deg).value)
-
-    if alt < 0:
-        continue
-
-    stars_mag.append(star["magnitude"])
-    stars_alt.append(alt)
-    stars_az.append(az)
+utcoffset = -4 * u.hour #EST
+#time = Time.now() - utcoffset
+t = Time(f"2025-2-7 00:00:00") - utcoffset
+observer = EarthLocation(lat=38.9864 * u.deg, lon=-76.8657 * u.deg, height=60)
+stars = get_stars(t, observer)
 
 fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
-
 circle = plt.Circle((0, 0), 1, transform=ax.transData._b, color="black")
 ax.add_artist(circle)
 
+print(stars[1])
+
+ax.grid(False)
+ax.set_ylim(0,1)
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+
 ax.scatter(
-    stars_az,
-    np.tan(np.pi/4 - np.deg2rad(np.array(stars_alt))/2),
-    s=100 * 10 ** (np.array(stars_mag) / -2.5),
+    stars[2],
+    np.tan(np.pi / 4 - np.deg2rad(stars[1]) / 2),
+    s=100 * 10 ** (stars[0] / -2.512),
     c="white",
-    marker=".",
+    marker  =".",
     linewidths=0,
     zorder=2
 )
-
-plt.axis('off')
 plt.show()
